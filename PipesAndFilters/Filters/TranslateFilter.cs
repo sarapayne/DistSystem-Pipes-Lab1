9f4b2970abdb30a00b6e81c03f28d2d310ebda0b
Sara Payne <sarapayne.uk@gmail.com>
2022-02-17 20:47:22 +0000

    added new translations for hext and binary

--- PipesAndFilters/Filters/TranslateFilter.cs
+++ PipesAndFilters/Filters/TranslateFilter.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Globalization;
 using System.Text;
 using PipesAndFilters.Messages;
 
@@ -10,43 +11,60 @@ namespace PipesAndFilters.Filters
         {
             if (message.Headers.ContainsKey("RequestFormat"))
             {
+                string[] bodyParts = message.Body.Split('-');
+                byte[] bytes = new byte[bodyParts.Length];
                 switch (message.Headers["RequestFormat"])
                 {
                     case "Bytes":
-                        string[] byteStrings = message.Body.Split('-');
-                        byte[] bytes = new byte[byteStrings.Length];
-                        for (int index = 0; index < byteStrings.Length; index++)
+                        
+                        for (int index = 0; index < bodyParts.Length; index++)
                         {
-                            bytes[index] = byte.Parse(byteStrings[index]);
+                            bytes[index] = byte.Parse(bodyParts[index]);
                         }
-
-                        message.Body = Encoding.ASCII.GetString(bytes);
+                        break;
+                    case "Hex":
+                        for (int index = 0; index < bodyParts.Length; index++)
+                        {
+                            bytes[index] = Convert.ToByte(bodyParts[index], 16);
+                        }
+                        break;
+                    case "Binary":
+                        for (int index = 0; index < bodyParts.Length; index++)
+                        {
+                            bytes[index] = Convert.ToByte(bodyParts[index], 2);
+                        }
                         break;
                     default:
                         break;
                 }
+                message.Body = Encoding.ASCII.GetString(bytes);
             }
             else if (message.Headers.ContainsKey("ResponseFormat"))
             {
-                switch (message.Headers["ResponseFormat"])
+                byte[] bytes = Encoding.ASCII.GetBytes(message.Body);
+                string messageBody = String.Empty;
+                for (int index = 0; index < bytes.Length; index++)
                 {
-                    case "Bytes":
-                        byte[] bytes = Encoding.ASCII.GetBytes(message.Body);
-                        string messageBody = String.Empty;
-                        for (int index = 0; index < bytes.Length; index++)
-                        {
+                    switch (message.Headers["ResponseFormat"])
+                    {
+                        case "Bytes":
                             messageBody += bytes[index].ToString();
-                            
-                            if (index < bytes.Length - 1)
-                            {
-                                messageBody += "-"; // - symbol is just a delimiter, could be anything
-                            }
-                        }
-                        message.Body = messageBody;
-                        break;
-                    default:
-                        break;
+                            break;
+                        case "Hex":
+                            messageBody += Convert.ToString(bytes[index], 16).PadLeft(8, '0');
+                            break;
+                        case "Binary":
+                            messageBody += Convert.ToString(bytes[index], 2).PadLeft(8, '0');
+                            break;
+                        default:
+                            break;
+                    }
+                    if (index < bytes.Length - 1)
+                    {
+                        messageBody += "-"; // - symbol is just a delimiter, could be anything
+                    }
                 }
+                message.Body = messageBody;
             }
             return message;
         }
